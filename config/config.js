ba30a7f919785d923b895b032b1c42e539611bf8
Vincent Choubard <vincent@dogbuddy.com>
2017-03-01 15:31:32 +0100

    Add config

---
+++ config/config.js
@@ -0,0 +1,113 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+var config = {
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
+
+	language: 'en',
+	timeFormat: 24,
+	units: 'metric',
+
+	modules: [
+		{
+			module: 'alert',
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: 'clock',
+			position: 'top_left'
+		},
+		{
+			module: 'calendar',
+			header: 'US Holidays',
+			position: 'top_left',
+			config: {
+				calendars: [
+					{
+						symbol: 'calendar-check-o ',
+						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
+					}
+				]
+			},
+			classes: 'Vincent'
+		},
+		{
+			module: 'compliments',
+			position: 'lower_third'
+		},
+		{
+			module: 'currentweather',
+			position: 'top_right',
+			config: {
+				location: 'New York',
+				locationID: '',  //ID from http://www.openweathermap.org
+				appid: 'YOUR_OPENWEATHER_API_KEY'
+			}
+		},
+		{
+			module: 'weatherforecast',
+			position: 'top_right',
+			header: 'Weather Forecast',
+			config: {
+				location: 'New York',
+				locationID: '5128581',  //ID from http://www.openweathermap.org
+				appid: 'YOUR_OPENWEATHER_API_KEY'
+			}
+		},
+		{
+			module: 'newsfeed',
+			position: 'bottom_bar',
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true
+			}
+		},
+		{
+			module: 'MMM-Facial-Recognition',
+			config: {
+				// 1=LBPH | 2=Fisher | 3=Eigen
+				recognitionAlgorithm: 1,
+				// Threshold for the confidence of a recognized face before it's considered a
+				// positive match.  Confidence values below this threshold will be considered
+				// a positive match because the lower the confidence value, or distance, the
+				// more confident the algorithm is that the face was correctly detected.
+				lbphThreshold: 50,
+				fisherThreshold: 250,
+				eigenThreshold: 3000,
+				// force the use of a usb webcam on raspberry pi (on other platforms this is always true automatically)
+				useUSBCam: true,
+				// Path to your training xml
+				trainingFile: 'modules/MMM-Facial-Recognition/training.xml',
+				// recognition intervall in seconds (smaller number = faster but CPU intens!)
+				interval: 2,
+				// Logout delay after last recognition so that a user does not get instantly logged out if he turns away from the mirror for a few seconds
+				logoutDelay: 15,
+				// Array with usernames (copy and paste from training script)
+				users: ["Vincent"],
+				//Module set used for strangers and if no user is detected
+				defaultClass: "default",
+				//Set of modules which should be shown for every user
+				everyoneClass: "everyone",
+				// Boolean to toggle welcomeMessage
+				welcomeMessage: true
+			}
+		}
+	]
+
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== 'undefined') {module.exports = config;}
